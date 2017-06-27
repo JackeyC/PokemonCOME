6161df5a24ee21c95a2d17f13f45e423682caba3
HP-Desktop <chaij@tcd.ie>
2017-06-27 08:43:30 +0100

    Revert "Auto stash before merge of "master" and "origin/master""
    This reverts commit 2f32d4daadd6baea7dd6fab6e32e815c824dfe58.

--- Assets/Project Assets/Scripts/Hololens/SpatialScanEffect.cs
+++ Assets/Project Assets/Scripts/Hololens/SpatialScanEffect.cs
@@ -2,36 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//namespace HoloToolkit.Unity.SpatialMapping
-//{
-    public class SpatialScanEffect : MonoBehaviour
-    {
-    //SpatialMappingObserver spatialMappingObserver;
-        float timeBetweenUpdates = 3.5f;
+public class SpatialScanEffect : MonoBehaviour {
 
-        public GameObject center;
-        public Material mat;
+    public Material mat;
 
-        float radius;
-        float updateTime;
+    float radius;
 
-        //void Start()
-        //{
-        //    spatialMappingObserver = GetComponent<SpatialMappingObserver>();
-        //}
+	void Update () {
 
-        void Update()
-        {
-            //if ((Time.unscaledTime - updateTime) >= timeBetweenUpdates)
-            //{
-            //    mat.SetVector("_Center", center.transform.position);
-            //    radius = 0;
-            //    updateTime = Time.unscaledTime;
-            //}
-
-            radius += Time.deltaTime;
-            radius %= 10;
-            mat.SetFloat("_Radius", radius);
-        }
-    }
-//}
+        radius += 0.5f * Time.deltaTime;
+        radius %= 10;
+        mat.SetFloat("_Radius", radius);
+	}
+}
