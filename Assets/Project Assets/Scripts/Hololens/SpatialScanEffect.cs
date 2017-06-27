12126e01d6b9a42152f2ea63a95b27fa1aa64ced
HP-Desktop <chaij@tcd.ie>
2017-06-27 18:25:49 +0100

    Auto stash before merge of "master" and "origin/master"

--- Assets/Project Assets/Scripts/Hololens/SpatialScanEffect.cs
+++ Assets/Project Assets/Scripts/Hololens/SpatialScanEffect.cs
@@ -2,16 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpatialScanEffect : MonoBehaviour {
+//namespace HoloToolkit.Unity.SpatialMapping
+//{
+    public class SpatialScanEffect : MonoBehaviour
+    {
+    //SpatialMappingObserver spatialMappingObserver;
+        float timeBetweenUpdates = 3.5f;
 
-    public Material mat;
+        public GameObject center;
+        public Material mat;
 
-    float radius;
+        float radius;
+        float updateTime;
 
-	void Update () {
+        //void Start()
+        //{
+        //    spatialMappingObserver = GetComponent<SpatialMappingObserver>();
+        //}
 
-        radius += 0.5f * Time.deltaTime;
-        radius %= 10;
-        mat.SetFloat("_Radius", radius);
-	}
-}
+        void Update()
+        {
+            //if ((Time.unscaledTime - updateTime) >= timeBetweenUpdates)
+            //{
+            //    mat.SetVector("_Center", center.transform.position);
+            //    radius = 0;
+            //    updateTime = Time.unscaledTime;
+            //}
+
+            radius += Time.deltaTime;
+            radius %= 10;
+            mat.SetFloat("_Radius", radius);
+        }
+    }
+//}
