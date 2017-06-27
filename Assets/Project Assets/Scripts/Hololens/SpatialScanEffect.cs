f5f0b45e3a389c9c0479763a394ac03a29a459dc
HP-Desktop <chaij@tcd.ie>
2017-06-27 19:22:56 +0100

    scan effect updated

--- Assets/Project Assets/Scripts/Hololens/SpatialScanEffect.cs
+++ Assets/Project Assets/Scripts/Hololens/SpatialScanEffect.cs
@@ -2,36 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//namespace HoloToolkit.Unity.SpatialMapping
-//{
-    public class SpatialScanEffect : MonoBehaviour
+public class SpatialScanEffect : MonoBehaviour
+{
+    public float timeBetweenUpdates = 3.5f;
+    public GameObject center;
+    public Material mat;
+
+    float radius;
+    float updateTime;
+
+    void Start()
     {
-    //SpatialMappingObserver spatialMappingObserver;
-        float timeBetweenUpdates = 3.5f;
-
-        public GameObject center;
-        public Material mat;
-
-        float radius;
-        float updateTime;
-
-        //void Start()
-        //{
-        //    spatialMappingObserver = GetComponent<SpatialMappingObserver>();
-        //}
-
-        void Update()
-        {
-            //if ((Time.unscaledTime - updateTime) >= timeBetweenUpdates)
-            //{
-            //    mat.SetVector("_Center", center.transform.position);
-            //    radius = 0;
-            //    updateTime = Time.unscaledTime;
-            //}
-
-            radius += Time.deltaTime;
-            radius %= 10;
-            mat.SetFloat("_Radius", radius);
-        }
+        SetCenter();
     }
-//}
+
+    void Update()
+    {
+        if ((Time.time - updateTime) >= timeBetweenUpdates)
+        {
+            SetCenter();
+        }
+
+        radius += Time.deltaTime;
+        radius %= 10;
+        mat.SetFloat("_Radius", radius);
+    }
+
+    void SetCenter()
+    {
+        mat.SetVector("_Center", center.transform.position);
+        radius = 0;
+        updateTime = Time.time;
+    }
+}
