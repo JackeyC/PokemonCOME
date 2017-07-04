5f0a5c0b41088af334f438093179031aac7526db
Jackey Chai <chaij@tcd.ie>
2017-07-04 19:05:52 +0100

    Pokeball Logic

--- Assets/Project Assets/Scripts/PokeBallLogic.cs
+++ Assets/Project Assets/Scripts/PokeBallLogic.cs
@@ -7,35 +7,40 @@ public class PokeBallLogic : MonoBehaviour {
     public Material material1;
     public Material material2;
     public float duration = 2;
+    public float despawnTime = 10;
 
     int range;
     Material[] rend = new Material[5];
 
+    
 
     void Start()
     {
-        range = GetComponent<Renderer>().materials.Length;
+        Destroy(gameObject, despawnTime);
+
+        //range = GetComponent<Renderer>().materials.Length;
         
-        for (int i = 0; i < range; i++)
-        {
-            rend[i] = GetComponent<Renderer>().materials[i];
-            rend[i] = material1;
-        }
+        //for (int i = 0; i < range; i++)
+        //{
+        //    rend[i] = GetComponent<Renderer>().materials[i];
+        //    rend[i] = material1;
+        //}
     }
-    void Update()
-    {
-        float lerp = Mathf.PingPong(Time.time, duration) / duration;
-        for (int i = 0; i < range; i++)
-        {
-            rend[i].Lerp(material1, material2, lerp);
-        }
-    }
+    //void Update()
+    //{
+    //    float lerp = Mathf.PingPong(Time.time, duration) / duration;
+    //    for (int i = 0; i < range; i++)
+    //    {
+    //        rend[i].Lerp(material1, material2, lerp);
+    //    }
+    //}
 
     void OnCollisionEnter(Collision col)
     {
         if (col.gameObject.tag == "Pokemon")
         {
             Destroy(col.gameObject, 2);
+            Destroy(gameObject, 2);
         }
     }
 }
